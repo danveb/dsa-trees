b6dfe51431eba0efe6e870258378d907ca74c4e4
Daniel Bae <danibae91@icloud.com>
2021-10-10 23:34:10 -0700

    submit working code for dsa-trees

--- tree.js
+++ tree.js
@@ -13,22 +13,119 @@ class Tree {
   }
 
   /** sumValues(): add up all of the values in the tree. */
+  /* QUEUE/STACK WORKS */ 
+
+  // sumValues() {
+  //   // initialize total and empty queue 
+  //   let total = 0
+  //   let queue = []
+  //   // push root node to the queue 
+  //   queue.push(this.root)
+
+  //   // while queue is not empty 
+  //   while(queue.length) {
+  //     let current = queue.shift() 
+  //     // if currrent 
+  //     if(current) {
+  //       total += current.val 
+  //       // for each child node push to queue 
+  //       for(let child of current.children) {
+  //         queue.push(child) 
+  //       }
+  //     }
+  //   }
+  //   return total 
+  // }
 
   sumValues() {
-    
+    // initialize total and empty stack []
+    let total = 0 
+    let stack = [] 
+    // push root node to the stack 
+    stack.push(this.root) 
+
+    // while loop, stack is NOT empty 
+    while(stack.length) {
+      let current = stack.pop() 
+      if(current) {
+        total += current.val 
+        for(let child of current.children) {
+          stack.push(child) 
+        }
+      }
+    }
+    return total; 
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
+  /* QUEUE/STACK WORKS */ 
+
+  // countEvens() {
+  //   // initialize count and empty queue 
+  //   let count = 0 
+  //   let queue = [] 
+  //   // push root node to the queue 
+  //   queue.push(this.root) 
+
+  //   while(queue.length) {
+  //     const current = queue.shift() 
+  //     // check if current
+  //     if(current) {
+  //       if(current.val % 2 === 0) {
+  //         count++
+  //       }
+  //       for(let child of current.children) {
+  //         queue.push(child)
+  //       }
+  //     } 
+  //   }
+  //   return count
+  // }
 
   countEvens() {
+    // initialize count and stack [] 
+    let count = 0 
+    let stack = [] 
+    // push root node to the stack 
+    stack.push(this.root) 
 
+    // while loop, stack is NOT empty 
+    while(stack.length) {
+      const current = stack.pop() 
+      if(current) {
+        if(current.val % 2 === 0) {
+          count += 1 
+        }
+        for(let child of current.children) {
+          stack.push(child) 
+        }
+      }
+    }
+    return count; 
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    // initialize count and empty queue 
+    let count = 0 
+    let queue = [] 
+    // push root node to the queue 
+    queue.push(this.root) 
 
+    while(queue.length) {
+      const current = queue.shift() 
+      if(current) {
+        if(current.val > lowerBound) {
+          count++ 
+        }
+        for(let child of current.children) {
+          queue.push(child)
+        }
+      }
+    }
+    return count 
   }
 }
 
